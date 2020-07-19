bd95bf317f7c0d9393a530c1a2470c858219918a
Sabindra340 <thisissabindra@gmail.com>
2020-07-18 20:08:30 -0400

    getting ready to puch catmat

---
+++ catmat.R
@@ -0,0 +1,27 @@
+## We are going to find the cache of the matrix
+
+CacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+    set <- function(y){
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(solveMatrix) inv <<- solveMatrix
+    getInverse <- function() inv
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+## Here we will find the inverse of the matrix above
+cacheInverse <- function(x, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)){
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setInverse(inv)
+    inv      
+}
